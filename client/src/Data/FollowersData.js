e27c14ba55a81f260e75e892886092b38fb4344e
Tomas1006 <60022837+tivanda@users.noreply.github.com>
2022-08-22 19:35:29 +0200

    add
    app

---
+++ client/src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/tomas.jpg"
+import img2 from "../img/img2.png"
+import img3 from "../img/img3.png"
+import img4 from "../img/img4.jpg"
+
+export const Followers = [
+    {name: "Petar Nikolic", username: "PetarNikolic", img: img1},
+    {name: "Dino Cuic", username: "DinoCuic", img: img2},
+    {name: "Nikola Semjaniv", username: "NikolaSemjaniv", img:img3},
+ {name: "Tihomir Orehovacki", username: "Tihomir Orehovacki", img:img4}
+];
---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Post.css'
+import Comment from '../../img/chat-bubble.png'
+import Share from '../../img/share.png'
+import Heart from '../../img/like.png'
+import NotLike from '../../img/notlike.png'
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { likePost } from '../../api/PostRequest'
+
+const Post = ({data})=> {
+    const{user} = useSelector((state)=>state.authReducer.authData)
+    
+
+
+    const [liked, setLiked] = useState(data.likes.includes(user._id))
+    const [likes, setLikes] = useState(data.likes.length)
+
+    const handleLike = () => {
+        setLiked((prev)=>!prev)
+        likePost(data._id, user._id)
+        liked? setLikes((prev)=>prev -1) : setLikes((prev)=> prev+1)
+    }
+
+    return (
+        <div className="Post"> 
+        <img src={data.image? process.env.REACT_APP_PUBLIC_FOLDER + data.image: ""} alt=""/>
+   <div className="detail">
+            <span><b>dr.{data.firstname}</b></span>
+            <br/>
+              <span>{data.desc}</span>
+        </div>
+
+        <div className="postReact">
+            <img src={liked?Heart: NotLike} alt="" style={{cursos: "pointer"}} onClick={handleLike} />
+            <img src={Comment} alt="" />
+        
+
+        </div>
+
+        <span style={{color: "var(--gray)", fontSize: '12px'}}>{likes} likes</span>
+
+     
+
+        </div>
+        
+        
+        
+        
+        )
+}
+
+export default Post
